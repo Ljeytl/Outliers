f706400d8c89921f27b5dc5fcd74357b863237af
Ljeytl <adysuri@gmail.com>
2020-02-24 15:46:58 -0500

    Basic Template App w/ UI assests
    2 scenes in scene folder contain main screen and game screen

---
+++ Assets/Scripts/playStartMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class playStartMenu : MonoBehaviour
+{
+
+    public void sceneLoader(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
